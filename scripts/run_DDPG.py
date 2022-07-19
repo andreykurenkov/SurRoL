521fbef32f6df8a523a4431da485e63d6f44728f
alinachouloute <70170664+alinachouloute@users.noreply.github.com>
2022-07-19 14:55:03 -0700

    Update run_DDPG.py

--- scripts/run_DDPG.py
+++ scripts/run_DDPG.py
@@ -1,19 +1,25 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', required = True)
+args = parser.parser.parse_arg()
+
 from stable_baselines3 import DDPG
 
 from surrol.gym import * 
 import gym 
 
-env = gym.make('NeedleReach-v0') # create one process and corresponding env
+env = gym.make('{args.name}') # create one process and corresponding env
 
 
 model = DDPG('MultiInputPolicy', env, verbose=1, tensorboard_log="./logs/")
 # Train the agent
 
-eval_env = gym.make('NeedleReach-v0')  # create one process and corresponding env
+eval_env = gym.make('{args.name}')  # create one process and corresponding env
 model.learn(total_timesteps=int(2e5), 
     log_interval=1, 
     eval_env=eval_env, 
     eval_freq=1000, 
     n_eval_episodes=5, 
-    tb_log_name='DDPG_needlereach', 
+    tb_log_name='DDPG_{args.name}', 
     eval_log_path="eval_logs")
